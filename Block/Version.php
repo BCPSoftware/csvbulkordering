dd272f3c6a55db080e6b20e274a2a82d26b3ab6e
Artem Naumov <a3.naumov@itransition.com>
2023-07-14 10:46:33 +0400

    OPD-5259 - Updated Version Block

--- Block/Version.php
+++ Block/Version.php
@@ -1,57 +1,72 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Oporteo\Csvorderupload\Block;
 
 use Magento\Backend\Block\Context;
 use Magento\Backend\Model\Auth\Session;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Config\Block\System\Config\Form\Fieldset;
 use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
 use Magento\Framework\Module\ModuleListInterface;
 use Magento\Framework\View\Helper\Js;
 use Magento\Framework\View\LayoutFactory;
-use Oporteo\Csvorderupload\Helper\Data;
 
-class Version extends \Magento\Config\Block\System\Config\Form\Fieldset
+class Version extends Fieldset
 {
+    /**
+     * @param Context $context
+     * @param Session $authSession
+     * @param Js $jsHelper
+     * @param ModuleListInterface $moduleList
+     * @param LayoutFactory $layoutFactory
+     * @param array $data
+     */
     public function __construct(
         Context $context,
         Session $authSession,
         Js $jsHelper,
         private ModuleListInterface $moduleList,
         private LayoutFactory $layoutFactory,
-        private Data $moduleHelper,
         array $data = []
     ) {
         parent::__construct($context, $authSession, $jsHelper, $data);
     }
 
-    public function render(AbstractElement $element)
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element): string
     {
         $html = $this->_getHeaderHtml($element);
 
-        $module = $this->_moduleList->getOne('Oporteo_Csvorderupload');
+        $module = $this->moduleList->getOne('Oporteo_Csvorderupload');
 
         $html .= $element->addField($module['name'], 'label', [
             'name'  => 'dummy',
             'label' => 'Version',
             'value' => $module['setup_version'],
-        ])->setRenderer($this->_getFieldRenderer())
-            ->toHtml();
+        ])->setRenderer($this->getFieldRenderer())->toHtml();
 
         $html .= $this->_getFooterHtml($element);
 
         return $html;
     }
 
-    private function _getFieldRenderer()
+    /**
+     * @return RendererInterface
+     */
+    private function getFieldRenderer(): RendererInterface
     {
-        if (empty($this->_fieldRenderer)) {
-            $layout = $this->_layoutFactory->create();
+        if (empty($this->fieldRenderer)) {
+            $layout = $this->layoutFactory->create();
 
-            $this->_fieldRenderer = $layout->createBlock(
-                \Magento\Config\Block\System\Config\Form\Field::class
-            );
+            $this->fieldRenderer = $layout->createBlock(Field::class);
         }
 
-        return $this->_fieldRenderer;
+        return $this->fieldRenderer;
     }
 }
