2ea1b67f07ead0dfce43105d49315e009fecfb42
Artem Naumov <a3.naumov@itransition.com>
2023-07-18 14:36:08 +0400

    Updated Config Observer

--- Observer/ConfigObserver.php
+++ Observer/ConfigObserver.php
@@ -1,22 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Oporteo\Csvorderupload\Observer;
 
-use Magento\Framework\App\Cache\Frontend\Pool;
 use Magento\Framework\App\Cache\TypeListInterface;
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
 
 class ConfigObserver implements ObserverInterface
 {
-
+    /**
+     * @param TypeListInterface $cacheTypeList
+     */
     public function __construct(
-        private TypeListInterface $cacheTypeList,
-        private Pool $cacheFrontendPool
+        private TypeListInterface $cacheTypeList
     ) {}
 
-    public function execute(EventObserver $observer)
+    /**
+     * @param EventObserver $observer
+     * @return void
+     */
+    public function execute(EventObserver $observer): void
     {
-        $this->_cacheTypeList->cleanType('full_page');
+        $this->cacheTypeList->cleanType('full_page');
     }
 }
